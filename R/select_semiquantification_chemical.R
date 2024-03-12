54fa1814a6d1c0a4ad5a1a393d236a0cc1d7ebb9
QizhiSu <sukissqz@gmail.com>
2024-03-12 23:24:48 +0800

    fix a bug in the select_std function

--- R/select_semiquantification_chemical.R
+++ R/select_semiquantification_chemical.R
@@ -17,8 +17,8 @@
 #' be at the end of the dataframe
 #' @param data_md1_col The column index where the molecular descriptor data
 #' starts in data_md
-#' @param top_k_md The number of top molecular descriptors to select (default
-#' is 20)
+#' @param top_npct_md The percentage of top molecular descriptors to select
+#' (default is 20)
 #'
 #' @return A metadata dataframe with the selected standard assigned to each
 #' substance and their similarity value
@@ -36,7 +36,11 @@ select_std <- function(
     std_md1_col,
     data_md,
     data_md1_col,
-    top_k_md = 20) {
+    top_npct_md = 20) {
+  # rename the columns to avoid errors in randomForest
+  colnames(std_md) <- gsub("-", "_", colnames(std_md))
+  colnames(data_md) <- gsub("-", "_", colnames(data_md))
+
   # construct the data for feature selection
   std4rf <- std_md[, c(std_res_col, std_md1_col:ncol(std_md))]
   colnames(std4rf)[1] <- "response" # rename the response column for later use
@@ -54,34 +58,49 @@ select_std <- function(
   # calculate the importance metric for each molecular descriptor
   vip_df <- randomForest::importance(rf_model) |> as.data.frame()
   vip_df <- vip_df[order(-vip_df$`%IncMSE`), ] # sort by decreasing importance
-  vip_df <- vip_df[1:top_k_md, ] # select top k molecular descriptors
+  # select top n per molecular descriptors
+  vip_df <- vip_df[1:ceiling(top_npct_md / 100 * nrow(vip_df)), ]
   vip_df$MD <- rownames(vip_df)
 
   # keep only top k molecular descriptors in the data and standard
-  data_md <- data_md[, c(1:data_md1_col, which(names(data_md) %in% vip_df$MD))]
-  std_md <- std_md[, c(1:std_md1_col, which(names(std_md) %in% vip_df$MD))]
+  data_md <- data_md[, c(
+    1:data_md1_col - 1,
+    which(names(data_md) %in% vip_df$MD)
+  )]
+  std_md <- std_md[, c(1:std_md1_col - 1, which(names(std_md) %in% vip_df$MD))]
 
   # create a metadata dataframe for the selected standard
-  data_meta <- data_md[, c(1:data_md1_col)]
+  data_meta <- data_md[, c(1:data_md1_col - 1)]
   data_meta$Standard <- NA
   data_meta$Similarity <- NA
 
-  cat("Assigning standard for target compounds...\n")
+  cat("Assigning standard for target compounds...\n\n")
   # calculate the similarity between the selected standard and each substance
   for (i in seq_len(nrow(data_md))) {
-    cat(i, "out of", nrow(data_md), "\n")
+    cat(i, "in", nrow(data_md), "\n")
     tmp <- vector() # create a temporary vector to store tempory similarity
+    data_meta$Standard[i] <- NA
+    data_meta$Similarity[i] <- NA
+
+    # take the molecular descriptor of the i substance
+    a <- data_md[i, data_md1_col:ncol(data_md)]
+    a_col <- names(a)[!is.na(a)] # remove empty molecular descriptor
+    a <- a[, a_col]
 
     # calculate the similarity one by one
     for (j in seq_len(nrow(std_md))) {
-      a <- data_md[i, data_md1_col:ncol(data_md)]
+      # take the molecular descriptor of the j standard
       b <- std_md[j, std_md1_col:ncol(std_md)]
-      tmp[j] <- proxy::simil(a, b) # calculate the similarity using the simil
-      data_meta$Standard[i] <- std_md$Name[which.max(tmp)]
-      data_meta$Similarity[i] <- max(tmp)
+      b <- b[, a_col] # keep only the common molecular descriptor
+      # Calculate similarity
+      tmp[j] <- proxy::simil(a, b)
     }
+
+    # assign the standard with the highest similarity
+    data_meta$Standard[i] <- std_md$Name[which.max(tmp)]
+    data_meta$Similarity[i] <- max(tmp)
   }
 
-  cat("Standard assignment completed.\n")
+  cat("\nStandard assignment completed.\n")
   return(data_meta)
 }
